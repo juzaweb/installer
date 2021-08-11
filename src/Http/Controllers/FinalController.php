7be0b23ff3526a60871a699af7b9028154ef8a6b
Thế Anh Đặng <anh.dang@3forcom.com.vn>
2021-08-11 19:04:48 +0700

    :tada: Begin a project

---
+++ src/Http/Controllers/FinalController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Juzaweb\Installer\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use Juzaweb\Installer\Events\LaravelInstallerFinished;
+use Juzaweb\Installer\Helpers\EnvironmentManager;
+use Juzaweb\Installer\Helpers\FinalInstallManager;
+use Juzaweb\Installer\Helpers\InstalledFileManager;
+
+class FinalController extends Controller
+{
+    /**
+     * Update installed file and display finished view.
+     *
+     * @param \Juzaweb\Installer\Helpers\InstalledFileManager $fileManager
+     * @param \Juzaweb\Installer\Helpers\FinalInstallManager $finalInstall
+     * @param \Juzaweb\Installer\Helpers\EnvironmentManager $environment
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function finish (
+        InstalledFileManager $fileManager,
+        FinalInstallManager $finalInstall,
+        EnvironmentManager $environment
+    )
+    {
+        $finalMessages = $finalInstall->runFinal();
+        $finalStatusMessage = $fileManager->update();
+
+        event(new LaravelInstallerFinished);
+
+        return view('installer::finished', compact(
+            'finalMessages',
+            'finalStatusMessage'
+        ));
+    }
+}
