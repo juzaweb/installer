8f93f6f6bcf78c7a90e048973765314bf7862edb
Thế Anh Đặng <anh.dang@3forcom.com.vn>
2021-08-15 13:37:31 +0700

    :construction: Main theme

--- src/Http/Controllers/FinalController.php
+++ src/Http/Controllers/FinalController.php
@@ -4,7 +4,6 @@ namespace Juzaweb\Installer\Http\Controllers;
 
 use Illuminate\Routing\Controller;
 use Juzaweb\Installer\Events\InstallerFinished;
-use Juzaweb\Installer\Helpers\EnvironmentManager;
 use Juzaweb\Installer\Helpers\FinalInstallManager;
 use Juzaweb\Installer\Helpers\InstalledFileManager;
 
@@ -13,16 +12,14 @@ class FinalController extends Controller
     /**
      * Update installed file and display finished view.
      *
-     * @param \Juzaweb\Installer\Helpers\InstalledFileManager $fileManager
-     * @param \Juzaweb\Installer\Helpers\FinalInstallManager $finalInstall
-     * @param \Juzaweb\Installer\Helpers\EnvironmentManager $environment
+     * @param InstalledFileManager $fileManager
+     * @param FinalInstallManager $finalInstall
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      * @throws \Throwable
      */
     public function finish (
         InstalledFileManager $fileManager,
-        FinalInstallManager $finalInstall,
-        EnvironmentManager $environment
+        FinalInstallManager $finalInstall
     )
     {
         $finalMessages = $finalInstall->runFinal();
